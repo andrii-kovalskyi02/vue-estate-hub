891f1afcbf0f1c46ccc20fbd18794284794375c3
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2024-01-11 23:29:16 +0100

    Created responsive header with routing, implemented custom icon component, and created a reusable function to determine the current client width

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <DesktopNav v-if="shouldRenderDesktopNav" />
+      <MobileNav v-else/>
+    </div>
+  </header>
+  <h1>{{ clientWidth }}</h1>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import useWindowWidth from '@/composables/useWindowWidth'
+import DesktopNav from './DesktopNav.vue'
+import MobileNav from './MobileNav.vue'
+
+const { clientWidth } = useWindowWidth()
+
+const shouldRenderDesktopNav = computed(() => clientWidth.value > 768)
+</script>
+
+<style scoped lang="scss">
+.header {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: $background-color-2;
+
+  @include onTablet {
+    position: initial;
+  }
+
+  &__content {
+    @include flexCenter;
+    justify-content: flex-start;
+    margin: 0 auto;
+    max-width: 1320px;
+    height: 70px;
+  }
+}
+</style>
---
+++ src/components/TheIcon.vue
@@ -0,0 +1,44 @@
+<template>
+  <span
+    class="icon"
+    :class="{ [`icon--${type}`]: true, [`icon--${type}-active`]: isActive }"
+    :aria-label="`${type} Icon`"
+  />
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+    type: String,
+    isActive: Boolean
+  }>(), {
+  isActive: () => false
+})
+</script>
+
+<style scoped lang="scss">
+.icon {
+  display: block;
+  width: 30px;
+  height: 30px;
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  &--house {
+    background-image: url(@/assets/images/icons/ic_mobile_navigation_home@3x.png);
+
+    &-active {
+      background-image: url(@/assets/images/icons/ic_mobile_navigation_home_active@3x.png);
+    }
+  }
+
+  &--about {
+    background-image: url(@/assets/images/icons/ic_mobile_navigation_info@3x.png);
+
+    &-active {
+      background-image: url(@/assets/images/icons/ic_mobile_navigation_info_active@3x.png);
+    }
+  }
+}
+</style>
